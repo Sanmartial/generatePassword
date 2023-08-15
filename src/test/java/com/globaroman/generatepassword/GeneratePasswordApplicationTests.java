c89740510fbee1d6583c10d5532395fd41761661
Roman Globa <globaroman@gmail.com>
2023-08-15 20:47:58 +0300

    Create project for generate passwords

---
+++ src/test/java/com/globaroman/generatepassword/GeneratePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaroman.generatepassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratePasswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
